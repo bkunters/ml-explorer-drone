0fec256887727a4ea96c3660ea746eb520c25b62
Janina Mattes <janina.alica.mattes@gmail.com>
2022-12-08 13:45:18 +0100

    src: Fix reward to go function

--- ppo_impl/ppo_torch/ppo.py
+++ ppo_impl/ppo_torch/ppo.py
@@ -22,8 +22,8 @@ import wandb
 
 # This is a TODO Section - please mark a todo as (done) if done
 # 0) Check current implementation against article: https://iclr-blog-track.github.io/2022/03/25/ppo-implementation-details/
-# 1) Adjust code for discrete domain --> CartPole
-# 2) Check code for continuous domain --> Pendulum
+# 1) Adjust code for discrete domain --> test with CartPole
+# 2) Check code for continuous domain --> test with Pendulum
 # 3) Implement Surrogate clipping loss
 # 4) Fix calculation of Advantage
 # 5) Check hyperparameters --> check overall implementation logic
@@ -128,15 +128,14 @@ class PPO_PolicyGradient:
         self.policyNet_optim = Adam(self.policyNet.parameters(), lr=self.lr) # Setup Policy Network (Actor) optimizer
         self.valueNet_optim = Adam(self.valueNet.parameters(), lr=self.lr)  # Setup Value Network (Critic) optimizer
 
-    def get_discrete_action_dist(self, obs):
+    def get_discrete_policy(self, obs):
         """Make function to compute action distribution in discrete action space."""
         # 2) Use Categorial distribution for discrete space
         # https://pytorch.org/docs/stable/distributions.html
         action_prob = self.policyNet.forward(obs) # query Policy Network (Actor) for mean action
         return Categorical(logits=action_prob)
 
-
-    def get_continuous_action_dist(self, obs):
+    def get_continuous_policy(self, obs):
         """Make function to compute action distribution in continuous action space."""
         # Multivariate Normal Distribution Lecture 15.7 (Andrew Ng) https://www.youtube.com/watch?v=JjB58InuTqM
         # fixes the detection of outliers, allows to capture correlation between features
@@ -147,7 +146,7 @@ class PPO_PolicyGradient:
         return multivariate_normal.MultivariateNormal(action_prob, covariance_matrix=cov_matrix)
 
     def get_action(self, dist):
-        """Sample a random action from distribution."""
+        """Make action selection function (outputs int actions, sampled from policy."""
         action = dist.sample()
         log_prob = dist.log_prob(action)
         return action, log_prob
@@ -158,7 +157,7 @@ class PPO_PolicyGradient:
 
     def step(self, obs):
         """ Given an observation, get action and probabilities from policy network (actor)"""
-        action_dist = self.get_continuous_action_dist(obs)
+        action_dist = self.get_continuous_policy(obs)
         action, log_prob = self.get_action(action_dist)
 
         # detach and convert to numpy array
@@ -168,13 +167,15 @@ class PPO_PolicyGradient:
 
     def rewards_to_go(self, rewards):
         """Calculate rewards to go to reduce the variance in the policy gradient"""
-        # Lecture 5, p. 17: UCB http://rail.eecs.berkeley.edu/deeprlcourse/static/slides/lec-5.pdf
+        # Lecture 5, p. 17 UCB Causality issue: http://rail.eecs.berkeley.edu/deeprlcourse/static/slides/lec-5.pdf
         # Open AI Documentation: https://spinningup.openai.com/en/latest/spinningup/rl_intro3.html#implementing-reward-to-go-policy-gradient
+        logging.info(f'Calculate rewards2go with batch rewards: {rewards}')
         amount = len(rewards)
         reward_to_go = np.zeros_like(rewards)
-        for i in reversed(range(amount)):
-            # TODO: Bug found. Check this function is a discount factor? 
-            reward_to_go[i] = rewards[i] + (reward_to_go[i+1] if i+1 < amount else 0)
+        for ep_reward in reversed(range(amount)): # iterate over rewards per episode
+            for i in reversed(range(ep_reward)):
+                pass
+            # reward_to_go[i] = rewards[i] + (reward_to_go[i+1] if i+1 < amount else 0)
         return torch.tensor(reward_to_go, dtype=torch.float)
 
     def advantage(self, rewards, values):
@@ -221,9 +222,9 @@ class PPO_PolicyGradient:
             episode_lengths_per_batch.append(episode + 1) # how long was the episode + 1 as we start at 0
             rewards_to_go_per_batch = self.rewards_to_go(rewards_per_batch)
 
-        return torch.tensor(observations_per_batch, dtype=torch.float), \
-                torch.tensor(actions_per_batch, dtype=torch.float), \
-                torch.tensor(log_probs_per_batch, dtype=torch.float), \
+        return torch.tensor(np.array(observations_per_batch), dtype=torch.float), \
+                torch.tensor(np.array(actions_per_batch), dtype=torch.float), \
+                torch.tensor(np.array(log_probs_per_batch), dtype=torch.float), \
                 torch.tensor(rewards_to_go_per_batch), \
                 episode_lengths_per_batch
 
@@ -305,10 +306,12 @@ if __name__ == '__main__':
 
     logging.info(f'env observation dim: {obs_dim}')
     logging.info(f'env action dim: {act_dim}')
+    
     # upper and lower bound describing the values our observations can take
     logging.info(f'upper bound for env observation: {env.observation_space.high}')
     logging.info(f'lower bound for env observation: {env.observation_space.low}')
     current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+   
     # Monitoring with W&B
     wandb.init(
     project=f'drone-mechanics-ppo',
