a067e6ced3098127443733bbc113770736f22838
explorerrrr <87107964+explorerrrr@users.noreply.github.com>
2023-01-30 04:26:24 +0800

    Update ppo_callback.py

--- PPO/ppo_baseline/ppo_callback.py
+++ PPO/ppo_baseline/ppo_callback.py
@@ -1,54 +1,66 @@
 import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import BaseCallback
-import wandb
 from stable_baselines3.common.logger import configure
-
+import wandb
+import csv
 
 class TrainingCallback(BaseCallback):
     """
     :param verbose: (int) Verbosity level 0: not output 1: info 2: debug
     """
 
-    def __init__(self, verbose=0):
+    def __init__(self, verbose=1):
         super(TrainingCallback, self).__init__(verbose)
         self.n_ep_rewards = []
         self.max_batch_size = 512
-        self.n_rollout_steps = 2200
-        self.t_episode = 0
+        self.n_rollout_steps = 2048
+        
         self.step = 0
-        self.mean_rewards = 0
+        self.t_steps = 0       #steps per rollout
+
         self.over_flag = False
+        self.f_csv = open('tmp/progress_callback.csv', 'w', newline='')
+        self.writer = csv.writer(self.f_csv, delimiter=',')
+        self.writer.writerow(['step', 'mean_reward', 'max_reward', 'min_reward'])
     def _on_step(self):
-        if(self.locals.get('n_steps')!=self.n_rollout_steps-1):
+        if(self.t_steps!=self.n_rollout_steps-1):
             if(self.locals.get('dones')):
-                # print(self.locals.get('n_steps'), self.locals.get('rewards'), self.locals.get('dones'), self.locals.get('infos'), self.locals.get('done'))
                 self.n_ep_rewards.append(self.locals.get('infos')[0].get('episode').get('r'))
                 if(self.over_flag):
-                    self.mean_rewards = np.mean(self.n_ep_rewards)
-                    print('------', self.step)
-                    print(self.mean_rewards)
-                    wandb.log({
-                        'mean_episodes_rewards': self.mean_rewards,
+                    mean_rewards = np.mean(self.n_ep_rewards)
+                    max_reward = max(self.n_ep_rewards)
+                    min_reward = min(self.n_ep_rewards)
+                    print('------', self.step+1, mean_rewards, max_reward, min_reward)
+                    self.writer.writerow([self.step+1, mean_rewards, max_reward, min_reward])
 
-                    }, step=self.step)
+                    # wandb.log({
+                    #     'mean_episodes_rewards': self.mean_rewards,
+                    #
+                    # }, step=self.step)
                     self.n_ep_rewards = []
                     self.over_flag = False
+                    self.t_steps = 0
         elif(self.locals.get('dones')):
             self.n_ep_rewards.append(self.locals.get('infos')[0].get('episode').get('r'))
-            self.mean_rewards = np.mean(self.n_ep_rewards)
-            print('------', self.step)
-            print(self.mean_rewards)
-            wandb.log({
-                'mean_episodes_rewards': self.mean_rewards,
-
-            }, step=self.step)
+            mean_rewards = np.mean(self.n_ep_rewards)
+            max_reward = max(self.n_ep_rewards)
+            min_reward = min(self.n_ep_rewards)
+            print('------', self.step+1, mean_rewards, max_reward, min_reward)
+            self.writer.writerow([self.step+1, mean_rewards, max_reward, min_reward])
+            # wandb.log({
+            #     'mean_episodes_rewards': self.mean_rewards,
+            #
+            # }, step=self.step)
             self.n_ep_rewards = []
+            self.t_steps = 0
         else:
             self.over_flag = True
 
         # print(self.locals)
+        self.f_csv.flush()
         self.step += 1
+        self.t_steps += 1
         return True  # returns True, training continues.
 
 
@@ -58,21 +70,20 @@ new_logger = configure(tmp_path, ["csv"])
 
 config = {
     "policy_type": "MlpPolicy",
-    "total_timesteps": 5_000_000,
+    "total_timesteps": 1_000_000,
     "env_name": "Pendulum-v1",
-    "n_steps": 2200,
+    "n_steps": 2048,
 }
 
-run = wandb.init(
-    project="log_test_zhengjie",
-    config=config,
-    entity='drone-mechanics',
-    name='stable_baselines3'
-)
+# run = wandb.init(
+#     project="log_test_zhengjie",
+#     config=config,
+#     entity='drone-mechanics',
+#     name='stable_baselines3'
+# )
 
 model = PPO(config["policy_type"], config["env_name"], n_steps=config["n_steps"], verbose=1)
 model.set_logger(new_logger)
 model.learn(total_timesteps=config["total_timesteps"], callback=TrainingCallback())
 
-run.finish()
-
+# run.finish()
